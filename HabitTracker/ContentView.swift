3da8afdd1a2f9fea86006630d675b127fa91513a
Max <maximdalat@gmail.com>
2023-05-31 12:18:10 -0400

    Added Todo's

--- HabitTracker/ContentView.swift
+++ HabitTracker/ContentView.swift
@@ -1,6 +1,34 @@
 import SwiftUI
 
-    //test
+/*
+ Stuff to do:
+ 
+ 1. Make it so that when you close the app it keeps it's progress [High priority bug]
+    A. Possible Database??
+    B. Save it locally
+    C. Even crazier, save it both locally and on a database
+ 2. Make todos
+    A. One time todo's like homework assignments for class and etc.
+    
+ 3. Just make a database and stop finding ways to avoid it
+    A. Using the database, make it possible to see assignments as a example on multiple devices with one leader phone, and other follower;
+ 4. Possibly try using the chromebook to host the database?
+    A. Find out a way to make it always on
+    B. Otherwise use my computer and turn off the; fan lights/replace them
+ 5. Admin Panel for Database / Admin login
+    A. Be careful when uploading to Git
+ 6. Make it possible to add different types of Blocks.
+    A. Ex. Todo's, reminders, homework assignments, tests.
+ 7. Login page
+    A. Add classes that you are in
+ 8. GooglePlay app [low priority]
+ 
+ 
+ 
+ */
+
+
+
 
 struct ContentView: View {
     struct Block: Identifiable {
